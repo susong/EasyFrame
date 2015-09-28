cf07654fd082fdc25a044319379043fa2a73576c
SuSong <751971697@qq.com>
2015-09-28 11:02:30 +0800

    MVP

---
+++ data/src/test/java/com/dream/data/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.dream.domain;
+
+import com.dream.data.Test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
---
+++ domain/src/main/java/com/dream/domain/MyClass.java
@@ -0,0 +1,4 @@
+package com.dream.domain;
+
+public class MyClass {
+}
