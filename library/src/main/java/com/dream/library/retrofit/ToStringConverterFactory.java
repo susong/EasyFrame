8749a7b508c57dd7b1b7b2500b8f491e9a7cd45c
SuSong <751971697@qq.com>
2015-10-26 15:43:56 +0800

    优化

---
+++ library/src/main/java/com/dream/library/retrofit/ToStringConverterFactory.java
@@ -0,0 +1,50 @@
+package com.dream.library.retrofit;
+
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.ResponseBody;
+
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
+import retrofit.Converter;
+
+/**
+ * Author:      SuSong
+ * Email:       751971697@qq.com | susong0618@163.com
+ * GitHub:      https://github.com/susong0618
+ * Date:        15/10/25 下午7:06
+ * Description: RetrofitTest
+ */
+public class ToStringConverterFactory extends Converter.Factory {
+
+    private static final MediaType MEDIA_TYPE = MediaType.parse("text/plain");
+
+    @Override
+    public Converter<ResponseBody, ?> fromResponseBody(Type type, Annotation[] annotations) {
+        if (String.class.equals(type)) {
+            return new Converter<ResponseBody, String>() {
+                @Override
+                public String convert(ResponseBody value) throws IOException {
+                    return value.string();
+                }
+            };
+        }
+        return null;
+    }
+
+    @Override
+    public Converter<?, RequestBody> toRequestBody(Type type, Annotation[] annotations) {
+        if (String.class.equals(type)) {
+            return new Converter<String, RequestBody>() {
+                @Override
+                public RequestBody convert(String value) throws IOException {
+                    return RequestBody.create(MEDIA_TYPE, value);
+                }
+            };
+        }
+        return null;
+    }
+
+}
---
+++ library/src/main/java/com/dream/library/utils/SimpleObserver.java
@@ -0,0 +1,27 @@
+package com.dream.library.utils;
+
+import rx.Observer;
+
+/**
+ * Author:      SuSong
+ * Email:       751971697@qq.com | susong0618@163.com
+ * GitHub:      https://github.com/susong0618
+ * Date:        15/10/21 下午7:44
+ * Description: SmallChaser
+ */
+public class SimpleObserver<T> implements Observer<T> {
+    @Override
+    public void onCompleted() {
+
+    }
+
+    @Override
+    public void onError(Throwable e) {
+
+    }
+
+    @Override
+    public void onNext(T t) {
+
+    }
+}
